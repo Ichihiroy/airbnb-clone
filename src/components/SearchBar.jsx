7591606cc7fe7f31fdd9b9c6177121f786f5b20e
Yunis Quliyev <realyanisquliyev@gmail.com>
2025-07-16 22:45:34 +0400

    feat: add framer-motion dependency and enhance SearchBar component interactivity

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,23 +2,41 @@ import { Search } from "lucide-react";
 import { useState } from "react";
 
 const SearchBar = () => {
-  const [active, setActive] = useState(false);
+  const [active, setActive] = useState({
+    where: false,
+    checkIn: false,
+    checkOut: false,
+    guests: false,
+    bar: false,
+  });
 
-  function handleClick(e) {
-    e.target.classList.add("bg-white", "rounded-full");
-    setActive(true);
+  function handleClick(type) {
+    // e.target.classList.add("bg-white", "rounded-full");
+    setActive({
+      bar: true,
+      where: type === "where" ? true : false,
+      checkIn: type === "checkIn" ? true : false,
+      checkOut: type === "checkOut" ? true : false,
+      guests: type === "guests" ? true : false,
+    });
   }
+
+  console.log(active);
 
   return (
     <div
       className={`flex
        ${
-         active ? "bg-zinc-300" : "bg-white"
+         active.bar ? "bg-gray-100" : "bg-white"
        } border my-3 items-center h-full border-zinc-200 rounded-full shadow-sm hover:shadow-md transition-shadow group`}
     >
       <div
-        onClick={(e) => handleClick(e)}
-        className="flex-1 ps-6 pe-0 hover:bg-zinc-200 hover:rounded-full py-3 h-[65px] border-gray-300 border-r-1 rounded-r-none group-hover:border-r-transparent"
+        onClick={() => handleClick("where")}
+        className={`flex-1 ps-6 pe-0  hover:rounded-full py-3 h-[65px] border-gray-100 border-r-1 group-hover:border-r-transparent ${
+          active.where
+            ? "bg-white rounded-full shadow-lg"
+            : "rounded-r-none hover:rounded-full hover:bg-zinc-200"
+        }`}
       >
         <div className="text-xs font-medium text-gray-900">Where</div>
         <input
@@ -27,22 +45,53 @@ const SearchBar = () => {
           className="w-full text-sm text-gray-600 placeholder-gray-400 outline-none"
         />
       </div>
-      <div className=" ps-6 pe-8 py-3 h-[65px]  border-gray-300 hover:bg-zinc-200 hover:rounded-full border-r-1 rounded-r-none group-hover:border-r-transparent">
+      <div
+        onClick={() => handleClick("checkIn")}
+        className={`ps-6 pe-8 py-3 h-[65px]  border-gray-100  border-r-1 group-hover:border-r-transparent
+          ${
+            active.checkIn
+              ? "bg-white rounded-full shadow-lg group-focus:border-r-transparent"
+              : "rounded-r-none hover:rounded-full hover:bg-zinc-200"
+          }
+          `}
+      >
         <div className="text-xs font-medium text-gray-900 mb-1">Check in</div>
         <div className="text-sm text-gray-400">Add dates</div>
       </div>
-      <div className="ps-6 pe-8 py-3 h-[65px] border-gray-300 hover:bg-zinc-200 hover:rounded-full border-r-1 rounded-r-none group-hover:border-r-transparent">
+      <div
+        onClick={() => handleClick("checkOut")}
+        className={`ps-6 pe-8 py-3 h-[65px] border-gray-100   border-r-1 group-hover:border-r-transparent
+          ${
+            active.checkOut
+              ? "bg-white rounded-full shadow-lg"
+              : "rounded-r-none hover:rounded-full hover:bg-zinc-200"
+          }
+          `}
+      >
         <div className="text-xs font-medium text-gray-900 mb-1">Check out</div>
         <div className="text-sm text-gray-400">Add dates</div>
       </div>
-      <div className="flex items-center justify-between h-[65px] flex-1 ps-6 pe-0.5 gap-3 border-gray-300 hover:bg-zinc-200 hover:rounded-full rounded-r-none">
+      <div
+        onClick={() => handleClick("guests")}
+        className={`flex items-center justify-between h-[65px] flex-1 ps-6 pe-0.5 gap-3 border-gray-100  
+          ${
+            active.guests
+              ? "bg-white rounded-full shadow-lg"
+              : "rounded-r-none hover:bg-zinc-200 hover:rounded-full"
+          }
+        `}
+      >
         <div className="flex flex-col items-start">
           <div className="text-xs font-medium text-gray-900">Who</div>
           <div className="text-sm text-gray-400">Add guests</div>
         </div>
         <div>
-          <button className="bg-red-500 text-white rounded-full p-4 m-2 hover:bg-red-600 transition-colors">
-            <Search size={16} />
+          <button
+            className={`bg-red-500 text-white rounded-full ${
+              active.bar ? "p-3" : "p-4"
+            } m-2 hover:bg-red-600 transition-colors flex items-center justify-center gap-2`}
+          >
+            <Search size={16} /> {active.bar ? "Search" : ""}
           </button>
         </div>
       </div>
