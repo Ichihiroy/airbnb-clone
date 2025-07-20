5ffe6babc3c6809cf28685230d6e6fac7892d443
Yunis Quliyev <realyanisquliyev@gmail.com>
2025-07-20 15:24:59 +0400

    feat: implement routing for Main and Details components in index file

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,24 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import Main from "./pages/Main.jsx";
+import Details from "./pages/Details.jsx";
+import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Main />,
+      },
+      {
+        path: "/details/:id",
+        element: <Details />,
+      },
+    ],
   },
 ]);
 
